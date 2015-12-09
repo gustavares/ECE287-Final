946e782b6ff636f6ebeb16de8860e24c39aa4851
Gustavo Tavares <gustavo.tavares2@live.com>
2015-12-09 00:40:23 -0500

    Initial Commit

---
+++ main.c
@@ -0,0 +1,16 @@
+int main() {
+	int i;
+	int j;
+	int a, b, c , d; // user inputs
+	int g, h; // led outputs
+
+	for (i = a; i < a+104; i++) {
+		for (j = 3; j < 104; j++) {
+			if(is_odd_parity(a)) {
+				g = exp(c, b);
+				g = pop_count(g+b);
+				h = pop_count(c+a);	
+			}
+		}
+	}
+}
