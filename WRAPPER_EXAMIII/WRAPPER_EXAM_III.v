1b725035b903ad9e4e138035c2cf4b7c2da115ab
Gustavo Tavares <gustavo.tavares2@live.com>
2015-12-10 01:56:03 -0500

    Quartus fails to compile due pin assignments

--- WRAPPER_EXAMIII/WRAPPER_EXAM_III.v
+++ WRAPPER_EXAMIII/WRAPPER_EXAM_III.v
@@ -248,19 +248,41 @@ input start;
 output done;
 reg done;
 
-reg pulseLoop2; // pulse to call the second loop
+reg pulseIncJ; // pulse to call the second loop
 reg [15:0]i;
+reg [15:0]j;
 reg [15:0]a_104; // first loop condition
 
-loop2 instantiate_loop2(clk, rst, a, b, c, d, g, h, pulseLoop2, done);
+reg pulseOdd;
+reg is_odd_parity;
 
-reg[1:0]S;
-reg[1:0]NS;
+reg [4:0]onesCount;
+
+reg [5:0]a_pos;
+reg [15:0]ex;
+reg [15:0]exCounter;
+reg exDone;
+
+reg [16:0]sumGB;
+reg [4:0]sumGB_pos;
+reg [4:0]popGB_counter;
+
+reg [16:0]sumCA;
+reg [4:0]sumCA_pos;
+reg [4:0]popCA_counter;
+
+reg[2:0]S;
+reg[2:0]NS;
 
 // STATES
 parameter WAIT_INPUTS = 2'd0,
    		  LOOP1 = 2'd1,
-   		  DONE = 2'd2;
+   		  INC_J = 3'd2,
+   		  ODD_PARITY = 3'd3,
+   		  EXP = 3'd4,
+   		  POP_GB = 3'd5,
+   		  POP_CA = 3'd6,
+   		  DONE = 3'd7;
 
 always @(posedge clk or negedge rst)
 begin
@@ -285,12 +307,50 @@ begin
 		end
 		LOOP1:
 		begin
-			if(i < a_104)
+			if(i < a_104 && pulseIncJ == 1'b0)
 				NS = LOOP1;	
 			else
-				NS = DONE;
+				NS = INC_J;
+		end
+		INC_J:
+		begin
+			if(j < 7'd104 && pulseOdd == 1'b0)
+				NS = INC_J;
+			else if(pulseOdd == 1'b1)
+				NS = ODD_PARITY;
+			else
+				NS = LOOP1;
+		end
+		ODD_PARITY:
+		begin
+			if(is_odd_parity == 1'b1)
+				NS = EXP;
+			else
+				NS = INC_J;
+		end
+		EXP:
+		begin
+			if(exCounter < b)
+				NS = EXP;
+			else
+				NS = POP_GB;
+		end
+		POP_GB:
+		begin
+			if(sumGB_pos < 5'd17)
+				NS = POP_GB;
+			else
+				NS = POP_CA;
+		end
+		POP_CA:
+		begin
+			if(sumCA_pos < 5'd17)
+				NS = POP_CA;
+			else
+				NS = INC_J;
 		end
 		DONE: NS = DONE;
+	endcase
 end
 
 
@@ -301,9 +361,29 @@ begin
 		g <= 16'd0;
 		h <= 16'd0;
 		done <= 1'b0;
+		
 		i <= a;
+	   	j <= 16'd3;
+
 	   	a_104 <= a + 16'd104;
-	   	pulseLoop2 <= 1'b0;
+
+	   	pulseIncJ <= 1'b0;
+		pulseOdd <= 1'b0;
+
+		onesCount <= 5'd0;
+		a_pos <= 5'd0;
+		
+		ex <= c;
+		exCounter <= 16'd0;
+		exDone <= 1'b0;
+		
+		sumGB <= g + b;
+		sumGB_pos <= 5'd0;
+		popGB_counter <= 5'd0;
+		
+		sumCA <= c + a;
+		sumCA_pos <= 5'd0;
+		popCA_counter <= 5'd0;
 	end
 	else
 	begin
@@ -311,10 +391,64 @@ begin
 		begin
 			case(S)
 				LOOP1:
+				begin
 					i <= i + 1;
-					pulseLoop2 <= 1'b1;
+					pulseIncJ <= 1'b1;
+				end
+				INC_J:
+				begin
+					j <= j + 1;
+					pulseOdd <= 1'b1;
+				end
+				ODD_PARITY: // is_odd_parity(a)
+				begin
+					if(a_pos < 5'd16)
+					begin
+						if(a[a_pos] == 1'b1)
+							onesCount <= onesCount + 1;
+						a_pos <= a_pos + 1;
+					end
+					else
+					begin
+						if((onesCount == 2) || (onesCount == 4) || (onesCount == 6) || (onesCount == 8) || (onesCount == 10) || (onesCount == 12) || (onesCount == 14) || (onesCount == 16))
+							is_odd_parity <= 1'b0;
+						else
+							is_odd_parity <= 1'b1;
+						pulseOdd <= 1'b0;
+					end
+				end
+				EXP: // g = exp(c, b)
+				begin
+					if(exCounter < b)
+						ex <= ex * c;
+					else
+						g <= ex;
+				end
+				POP_GB: // g = pop_count(g+b)
+				begin
+					if(sumGB_pos < 5'd17)
+					begin
+						if(sumGB[sumGB_pos] == 1'b1)
+							popGB_counter <= popGB_counter + 1;
+						sumGB_pos <= sumGB_pos + 1;
+					end
+					else
+						g <= popGB_counter;
+				end
+				POP_CA: // h = pop_count(c+a)
+				begin
+					if(sumCA_pos < 5'd17)
+					begin
+						if(sumCA[sumCA_pos] == 1'b1)
+							popCA_counter <= popCA_counter + 1;
+						sumCA_pos <= sumCA_pos + 1;
+					end
+					else
+						h <= popCA_counter;
+				end
 				DONE:
 					done <= 1'b1;
+			endcase
 		end
 	end
 end
